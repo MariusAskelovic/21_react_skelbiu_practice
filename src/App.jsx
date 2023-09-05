0d90f1892d2c8c1984070715e19a413963293fe6
MariusAskelovic <72157256+MariusAskelovic@users.noreply.github.com>
2023-09-05 09:56:10 +0300

    protecting routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,46 @@
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import MyAccountPage from './pages/MyAccountPage';
 import Header from './components/layout/Header';
 import CreateAdd from './pages/CreateAdd';
 import LoginPage from './pages/LoginPage';
+import { useAuth } from './store/AuthProvider';
 
 export default function App() {
+  const ctx = useAuth();
+  ctx.isUserLoggedIn;
   return (
     <div>
       <Header />
       <Routes>
         <Route path='/' element={<HomePage />} />
-        <Route path='/my-ads' element={<MyAccountPage />} />
-        <Route path='/create-ad' element={<CreateAdd />} />
+        <Route
+          path='/my-ads'
+          element={
+            ctx.isUserLoggedIn ? <MyAccountPage /> : <Navigate to={'/login'} />
+          }
+        />
+        <Route
+          path='/create-ad'
+          element={
+            ctx.isUserLoggedIn ? <CreateAdd /> : <Navigate to={'/login'} />
+          }
+        />
         <Route path='/login' element={<LoginPage />} />
+        <Route
+          path='*'
+          element={
+            <div>
+              <h1
+                className='text-9xl ml-auto
+              mr-auto text-red-800'
+              >
+                404
+              </h1>
+              <p className='uppercase'>Page not found</p>
+            </div>
+          }
+        />
       </Routes>
     </div>
   );
